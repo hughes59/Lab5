b492b7eadc09a26be46e054a9678d59c87842a1f
StvnDscSU <48736008+StvnDscSU@users.noreply.github.com>
2020-11-16 14:42:54 -0500

    Nested the if-statement

--- src/hellojava/Hello.java
+++ src/hellojava/Hello.java
@@ -17,9 +17,8 @@ public class Hello {
     public static void main(String[] args) {
         String name = "World";
 		
-		if ( argv.length != 0 ){
+		if ( argv.length != 0 )
 			name = argv[0];
-		}
 		
 		System.out.println( "Hello, " + name + "!" );
     }
